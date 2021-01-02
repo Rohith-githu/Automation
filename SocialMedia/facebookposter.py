59a573fffddaa3a0b5d7c6e41b21c1a2500a11dc
Rohith-githu <rohithanumalasetty@gmail.com>
2021-01-02 12:55:22 +0530

    poster new line for link preview

--- SocialMedia/facebookposter.py
+++ SocialMedia/facebookposter.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 import time
 import pyautogui
-
+link = input('Enter the link :\n')
 post = input('Enter the Post :\n')
 
 # block_notify = 
@@ -34,7 +34,7 @@ time.sleep(5)
 
 writer_info = driver.find_element_by_xpath('//*[@id="mount_0_0"]/div/div[1]/div[1]/div[4]/div/div/div[1]/div/div[2]/div/div/div/form/div/div[1]/div/div/div[2]/div[1]/div[1]/div/div[1]/div/div/div/div/div[2]/div/div/div/div')
 # writer_info.click()
-writer_info.send_keys(post)
+writer_info.send_keys(f'{link}\n{post}')
 time.sleep(5)
 post_button = driver.find_element_by_xpath('//*[@id="mount_0_0"]/div/div[1]/div[1]/div[4]/div/div/div[1]/div/div[2]/div/div/div/form/div/div[1]/div/div/div[3]/div[2]/div')
 post_button.click()
@@ -43,3 +43,6 @@ time.sleep(5)
 
 like_button = driver.find_element_by_xpath('//*[@id="mount_0_0"]/div/div[1]/div[1]/div[3]/div/div/div[1]/div[1]/div/div[2]/div/div/div[3]/div/div[4]/div/div[1]/div/div/div/div/div/div/div/div/div/div/div[2]/div/div[4]/div/div/div[1]/div/div/div/div[1]/div[1]/div[1]')
 like_button.click()
+
+time.sleep(10)
+driver.quit()
